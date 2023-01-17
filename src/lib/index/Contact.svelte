806a782c04caca817d6d1e44dc9f5858b8b2e0f7
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2023-01-17 18:35:25 +0000

    test 4

--- src/lib/index/Contact.svelte
+++ src/lib/index/Contact.svelte
@@ -28,7 +28,7 @@
 
 <style lang="postcss">
     section {
-        @apply flex items-center justify-center flex-col gap-7 px-5;
+        @apply flex items-center justify-center flex-col gap-10 px-10;
     }
 
     section > h1, section > p {
