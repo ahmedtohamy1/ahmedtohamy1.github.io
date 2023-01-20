1e67fc3160e7914acf5f2027dbe4573b5aec191c
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2023-01-20 17:59:30 +0000

    proj tt

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -42,7 +42,7 @@
 
 <style lang="postcss">
     main .project {
-        @apply w-screen max-w-md py-6 px-8 border-[2px] rounded-lg text-left
+        @apply py-6 px-8 border-[1px] rounded-lg text-left
             border-white dark:border-black/20
             bg-white/30 dark:bg-black/30;
             /* bg-white/20 dark:bg-gray-900 border-black/5
