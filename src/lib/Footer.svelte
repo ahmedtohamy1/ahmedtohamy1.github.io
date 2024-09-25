2ac5af5747b052e0648d5fe5b24796dd3691d30c
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2024-09-25 14:26:33 +0300

    Update Src

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 </script>
 
-<footer class="py-4 font-inter" id="footer">
-    <p>&copy; <span class="font-dela">Ahmed Tohamy</span> 2022</p>
-    <a href={"#"}>Back to top</a>
+<footer id="footer">
+	<p>&copy; <span class="font-dela">Ahmed Tohamy</span> 2024</p>
+	<a href={"#"}>Back to top</a>
 </footer>
 
 <style lang="postcss">
-    footer {
-        @apply flex flex-col gap-y-2
-    }
+	footer {
+		@apply flex flex-col gap-y-2 py-8 font-inter
+			bg-white/50 border-t-[1px] border-white;
+	}
+
+	:global(head.dark ~ body) footer {
+		@apply bg-black/40 border-white/20;
+	}
 </style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,27 @@
-<script lang="ts">
+<script>
 	import Shader from "$lib/Shader.svelte";
 
 	import "@fontsource/dela-gothic-one/latin.css";
 	import "@fontsource/inter/latin.css";
-    import "../app.css";
+	import "../app.css";
 
-    import vertexShader from "$lib/shaders/snow.vert.js";
-    import fragmentShader from "$lib/shaders/snow.frag.js";
+	import vertexShader from "$lib/shaders/halloween.vert.js";
+	import fragmentShader from "$lib/shaders/halloween.frag.js";
 </script>
 
 <svelte:head>
-    <script>
-        var prefered = localStorage.getItem("theme");
-        var darkMode = prefered ? prefered === "dark" :
-            window.matchMedia("(prefers-color-scheme: dark)").matches;
-        darkMode ? document.head.classList.add("dark") : 
-            document.head.classList.remove("dark");
-    </script>
+	<script>
+		var prefered = localStorage.getItem("theme");
+		var darkMode = prefered
+			? prefered === "dark"
+			: window.matchMedia("(prefers-color-scheme: dark)").matches;
+		darkMode ? document.head.classList.add("dark") : document.head.classList.remove("dark");
+	</script>
 </svelte:head>
 
-
 <div id="bg">
-    <Shader {vertexShader} {fragmentShader} />
-	<div/>
+	<Shader {vertexShader} {fragmentShader} />
+	<div />
 </div>
 
 <div id="root">
@@ -31,6 +30,6 @@
 
 <style lang="postcss">
 	#bg div {
-		@apply backdrop-blur-lg fixed top-0 left-0 w-screen h-screen bg-white/60 -z-10 dark:bg-black/80;
+		@apply fixed top-0 left-0 w-screen h-screen bg-white/90 -z-10 dark:bg-black/80;
 	}
 </style>
